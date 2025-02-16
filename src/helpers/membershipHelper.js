4b0a6eba5812f74a5b0fa13c592bfffd1640a484
PietroDeBianchi <pietro.debianchi@shadapps.it>
2025-02-16 23:00:57 +0100

    fix: :construction: Added user update to membership creation

--- src/helpers/membershipHelper.js
+++ src/helpers/membershipHelper.js
@@ -1,22 +1,28 @@
 const Membership = require("../models/Membership");
+const User = require("../models/User");
 
 const MembershipController = {
-    // CREATE GROUP
+    // CREATE MEMBERSHIP
     async createMembership(user_id, gruppo_id, is_admin) {
         try {
-            // Creazione nuovo Membership
             const newMembership = new Membership({
                 gruppo_id,
                 user_id,
                 is_admin: is_admin ?? false,
             });
-            // Salvataggio nel database
             await newMembership.save();
-            // Restituisce l'ID della membership appena creata
+            const user = await User.findById(user_id);
+            if (!user) {
+                console.error("Errore: Utente non trovato");
+                return null;
+            }
+            user.memberships = user.memberships || [];
+            user.memberships.push(newMembership._id);
+            await user.save()
             return newMembership._id || null;
         } catch (error) {
             console.error("Errore nella creazione della membership:", error);
-            return null; // Evita crash, puoi gestire errore nel controller
+            return null; 
         }
     },
 };
